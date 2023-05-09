b02b4c2b080ce574eee1ebc491f20972ab1e843e
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-05-09 13:42:45 +0000

    added cors configurations Asynchronously

--- server.js
+++ server.js
@@ -8,14 +8,29 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//configuring CORS Asynchronously
+const whitelist = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
+
 //adding dependencies
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(cors());
+
 app.use(morgan("combine"));
 
 app.use("", require("./api/routes/main"));
 
+//mongoDB connection
 mongoose
   .connect(
     "mongodb+srv://admin:12345@mongodb.u5d7p4i.mongodb.net/?retryWrites=true&w=majority"
