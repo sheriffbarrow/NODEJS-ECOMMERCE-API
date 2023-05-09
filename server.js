6eae2331ee38c468475bf7ae40013fff514fa06c
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-05-09 17:19:39 +0000

    removed router instances and dependencies from server connection

--- server.js
+++ server.js
@@ -1,38 +1,10 @@
-const express = require("express");
+const http = require("http");
+const app = require("./app");
 require("dotenv").config();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const app = express();
-
-//configuring CORS Asynchronously
-const whitelist = [
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
-  "http://example:3000",
-];
-const corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header("origin")) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
-
-app.use(cors(corsOptionsDelegate));
-
-//adding dependencies
-app.use(helmet());
-app.use(bodyParser.json());
-
-app.use(morgan("combine"));
-
-app.use("", require("./api/routes/main"));
+//server connection instance
+const server = http.createServer(app);
 
 //mongoDB connection
 mongoose
@@ -41,6 +13,6 @@ mongoose
   )
   .then(() => console.log("connected to mongodb"));
 
-app.listen(process.env.SERVER_PORT, () => {
+server.listen(process.env.SERVER_PORT, () => {
   console.log(`server listining on port ${process.env.SERVER_PORT}`);
 });
