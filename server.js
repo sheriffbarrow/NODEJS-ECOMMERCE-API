f8361abc51264f16d20e6a9f9ecafed3b09059a1
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-05-17 06:38:47 +0000

    used environment variables for mongo connection string

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const mongoose = require("mongoose");
 const server = http.createServer(app);
 
 //mongoDB connection
-mongoose
-  .connect(
-    "mongodb+srv://admin:12345@mongodb.u5d7p4i.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("connected to mongodb"));
+async function dbConnection() {
+  const response = await mongoose.connect(process.env.MONGO_URL);
+  if (!response) {
+    console.log(`async connection to db failed with port: ${process.PORT}`);
+  } else {
+    console.log("async connection made");
+  }
+}
+dbConnection();
 
-server.listen(process.env.SERVER_PORT, () => {
+server.listen(process.env.SERVER_PORT || 4000, () => {
   console.log(`server listining on port ${process.env.SERVER_PORT}`);
 });
